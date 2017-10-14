1e1401a32e9f860764065a3533cfaf3444a35be6
Sergio Gallardo Sales <gallardo91_@hotmail.com>
2017-10-14 11:33:18 +0200

    Change Color from Settings

--- features/deckPicker/DeckPicker.js
+++ features/deckPicker/DeckPicker.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Picker, Text, View } from 'react-native';
+import { connect } from 'react-redux';
 
-export default class DeckPicker extends Component {
+import { changeColorDeck } from '../settings/actions';
+
+class DeckPicker extends Component {
   render() {
     return (
       <View>
         <Text>Color del Mazo</Text>
           <Picker
-            onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}>
+            selectedValue={this.props.getColor(this.props.index)}
+            onValueChange={(itemValue) => this.props.onColorChange(this.props.index, itemValue)}>
             <Picker.Item label="Blanco" value="#FFFFE1" />
             <Picker.Item label="Azul" value="#A0DDF9" />
             <Picker.Item label="Negro" value="#C4B8B6" />
@@ -18,3 +22,13 @@ export default class DeckPicker extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  getColor: (index) => state.settings[index].color
+});
+
+const mapDispatchToProps = dispatch => ({
+    onColorChange: (index, newColor) => dispatch(changeColorDeck({ index: index, newColor: newColor })),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DeckPicker);
